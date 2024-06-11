7d54af7b27bb17ec7295e5449b35bf07ebf012cf
Gabriel Alencar <pixcel97@gmail.com>
2024-06-11 15:27:15 -0300

    Atualizado pipeline

--- ci/scripts/up_env.sh
+++ ci/scripts/up_env.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+sudo sysctl -w fs.file-max=32768
+
 docker compose -f ci/docker/docker-compose.yml -p protheus up -d
+
+sleep 10
+
+docker compose -p protheus exec -i appserver /bin/sh -c "ls -lah /opt/totvs/protheus/apo/"
+docker compose -p protheus exec -i appserver /bin/sh -c "ls -lah /opt/totvs/protheus/protheus_data/systemload/"
 
 echo "Protheus Started"
